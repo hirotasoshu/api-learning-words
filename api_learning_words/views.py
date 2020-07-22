b7b9e5865642aeeced52bebbc107ea034724424d
hirotasoshu <maksim.zaiakin@urfu.me>
2020-07-22 13:08:32 +0500

    Add auto prefetching

--- api_learning_words/views.py
+++ api_learning_words/views.py
@@ -1,9 +1,10 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from django_auto_prefetching import AutoPrefetchViewSetMixin
 from .serializers import CategorySerializer
 from .models import Category
 
 
-class CategoryViewSet(ReadOnlyModelViewSet):
+class CategoryViewSet(AutoPrefetchViewSetMixin, ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
