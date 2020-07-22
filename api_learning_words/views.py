4ebba0a74d3fd5e5ec7da4307a69bcd03679604e
hirotasoshu <maksim.zaiakin@urfu.me>
2020-07-22 14:12:36 +0500

    Change viewsets to GenericViewSet

--- api_learning_words/views.py
+++ api_learning_words/views.py
@@ -1,15 +1,16 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
+from rest_framework.mixins import ListModelMixin
 from django_auto_prefetching import AutoPrefetchViewSetMixin
 from .serializers import *
 from .models import *
 
 
-class CategoryViewSet(ReadOnlyModelViewSet):
+class CategoryViewSet(ListModelMixin, GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class LevelViewSet(ReadOnlyModelViewSet):
+class LevelViewSet(ListModelMixin, GenericViewSet):
     queryset = Level.objects.all()
     serializer_class = LevelSerializer
 
