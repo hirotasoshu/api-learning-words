c3ff57519af66e2bf176fbe5d58c20967e773ddb
hirotasoshu <maksim.zaiakin@urfu.me>
2020-07-24 06:55:54 +0500

    Fixed request.META in CheckApiKeyAuth

--- api_learning_words/permissions.py
+++ api_learning_words/permissions.py
@@ -5,6 +5,6 @@ from rest_framework.permissions import BasePermission
 class CheckApiKeyAuth(BasePermission):
     def has_permission(self, request, view):
         # Secret should be in request headers to authenticate requests
-        api_secret = request.META['HTTP_SECRET']
+        api_secret = request.META.get('HTTP_SECRET')
         return api_secret == settings.API_SECRET
 
