75ab649f4851afafac497e61b06a6c4a821ffd87
hirotasoshu <maksim.zaiakin@urfu.me>
2020-08-06 18:49:55 +0500

    Move logging settings from base to prod

--- app/learning_words/settings/base.py
+++ app/learning_words/settings/base.py
@@ -142,22 +142,3 @@ REST_FRAMEWORK = {
     ]
 }
 
-## Logging configuration
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'debug.log'),
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
--- app/learning_words/settings/prod.py
+++ app/learning_words/settings/prod.py
@@ -11,6 +11,26 @@ MEDIA_ROOT = '/media_files/'
 SECRET_KEY = os.environ.get('SECRET_KEY', default='super-secret-key')
 API_SECRET = os.environ.get('API_SECRET', default='md5-hash-doubletapp')
 
+## Logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 ALLOWED_HOSTS = [
     "127.0.0.1"
 ]
